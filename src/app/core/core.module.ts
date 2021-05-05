7ce0128f05b749d38a38423704766b9299a0becb
imrylevysadan <imrysason@gmail.com>
2021-05-05 15:32:31 +0300

    Add header actions component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,14 @@ import {CoreComponent} from './core.component';
 import {CoreRoutingModule} from './core-routing.module';
 import {HeaderComponent} from './components/header/header.component';
 import {SharedModulesModule} from '../shared/modules/shared-modules.module';
+import { HeaderActionsComponent } from './components/header-actions/header-actions.component';
 
 
 @NgModule({
   declarations: [
     CoreComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeaderActionsComponent
   ],
   imports: [
     CommonModule,
