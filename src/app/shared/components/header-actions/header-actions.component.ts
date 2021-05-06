7d2a840d9e1ef02697fcaf688da13e8b98403540
imrylevysadan <imrysason@gmail.com>
2021-05-06 13:34:40 +0300

    Add "add" button

--- src/app/shared/components/header-actions/header-actions.component.ts
+++ src/app/shared/components/header-actions/header-actions.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {Category} from '../../models/category';
 
 @Component({
   selector: 'app-header-actions',
   templateUrl: './header-actions.component.html',
-  styleUrls: ['./header-actions.component.scss']
+  styleUrls: ['./header-actions.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderActionsComponent implements OnInit {
+  @Input() categories: Category[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  get isNoCategories(){
+    return !this.categories || this.categories && this.categories.length === 0;
   }
 
 }
